1a3987b3baed64f67fe91764ab3b9e436f64e6a0
ITK13201 <57588603+ITK13201@users.noreply.github.com>
2020-05-31 00:07:21 +0900

    結合の実装完了

--- ControlPDF.py
+++ ControlPDF.py
@@ -1,14 +1,28 @@
 import PyPDF2, os
 
 def main():
-    print('Combine or Split [c/s]: ', end='')
-    cmd = input().lower()
+    while True:
+        print('Combine or Split [c/s]: ', end='')
+        cmd = input().lower()
+        if cmd == 'c' or cmd == 's':
+            break
+        else:
+            print('No such command. Input again.')
+    
 
     '''PDFファイルの結合'''
     if cmd == 'c':
         pdf_files = []
-        print('How many pdfs?: ', end='')
-        num = int(input())
+        
+        while True:
+            try:
+                print('How many pdfs?: ', end='')
+                num = int(input())
+            except ValueError as err:
+                print('invalid literal for int(). Input again.')
+            else:
+                break
+        
         for i in range(num):
             while True:
                 print('pdf[{}]: '.format(i+1), end='')
@@ -26,6 +40,17 @@ def main():
 
             pdf_files.append(filename)
         
+        # ファイルの書き込み
+        pdf_writer = PyPDF2.PdfFileWriter()
+        # 全てのpdfファイルをループする
+        for filename in pdf_files:
+            pdf_file_obj = open(filename, 'rb')
+            pdf_reader = PyPDF2.PdfFileReader(pdf_file_obj)
+            # 全てのページをループする
+            for page_num in range(pdf_reader.numPages):
+                page_obj = pdf_reader.getPage(page_num)
+                pdf_writer.addPage(page_obj)
+        
         # 作成するpdfの名前
         print('created_filename: ', end='')
         created_filename = input()
@@ -35,13 +60,14 @@ def main():
         else:
             created_filename += '.pdf'
 
-        pdf_writer = PyPDF2.PdfFileWriter()
-        # 全てのpdfファイルをループする
-        for filename in pdf_files:
-            pdf_file_obj = open(created_filename, 'wb')
-            pdf_reader = PyPDF2.PdfFileReader(pdf_file_obj)
-        
-        
+        # 結合したPDFをファイルに保存する．
+        pdf_output_file = open(created_filename, 'wb')
+        pdf_writer.write(pdf_output_file)
+        pdf_output_file.close()
+
+        print('Completed!')
+    
+    
 
 if __name__ == "__main__":
     main()
